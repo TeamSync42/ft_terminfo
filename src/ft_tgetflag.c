4899ff6b0aa93cb50ec9db5392f5014862ac8c0a
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-20 11:12:22 +0200

    refactor(terminfo): update ft_tgetflag to use session-based lookup

--- src/ft_tgetflag.c
+++ src/ft_tgetflag.c
@@ -6,15 +6,27 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:48:31 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/19 17:06:55 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/20 11:11:28 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_terminfo.h"
 #include "ft_terminfo_internal.h"
 
-int32_t	ft_tgetflag(const char *id)
+int32_t	ft_tgetflag(t_terminfo_session *session, const char *id)
 {
-	(void)id;
+	int32_t				idx;
+	t_terminfo_entry	*entry;
+
+	if (!session || !session->current_terminfo)
+		return (0);
+	idx = ft_find_capability_index(id, CAP_TYPE_BOOL);
+	if (idx == -1)
+		return (0);
+	entry = session->current_terminfo;
+	if (idx >= MAX_BOOLS_CAPS)
+		return (0);
+	if (entry->bools[idx >> 3] & (1 << idx % 8))
+		return (1);
 	return (0);
 }
