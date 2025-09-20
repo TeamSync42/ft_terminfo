98b735d5ba03b80ed73cd16c46d958659eb219fc
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-20 07:15:02 +0200

    feat(terminfo): implement numeric capability lookup in ft_tgetnum

--- src/ft_tgetnum.c
+++ src/ft_tgetnum.c
@@ -6,8 +6,23 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:48:38 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/18 12:16:55 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/20 00:45:48 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_terminfo.h"
+#include "ft_terminfo_internal.h"
+
+int32_t	ft_tgetnum(t_terminfo_session *session, const char *id)
+{
+	t_terminfo_entry	*entry;
+	int32_t				cap_idx;
+
+	if (!session || !session->current_terminfo)
+		return (-1);
+	entry = session->current_terminfo;
+	cap_idx = ft_find_capability_index(id, CAP_TYPE_NUM);
+	if (cap_idx == -1)
+		return (-1);
+	return (entry->nums[cap_idx]);
+}
