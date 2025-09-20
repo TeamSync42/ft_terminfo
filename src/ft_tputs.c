5d51812676e05be7838b7391561e22d3b4216c85
Riyane El Qoqui <riyane.dev@gmail.com>
2025-09-20 15:34:09 +0200

    refactor(terminfo): extract delay parsing logic into parse_delay function

--- src/ft_tputs.c
+++ src/ft_tputs.c
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 11:48:51 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/09/20 15:28:04 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/09/20 15:33:48 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,35 @@
 
 #include "ft_terminfo.h"
 
-int32_t	ft_tputs(t_terminfo_session *session, const char *str, int32_t affcnt,
-			int32_t (*putc)(int32_t))
+static const char	*parse_delay(const char *p, const int32_t affcnt)
 {
-	const char *p;
 	int32_t		delay_ms;
+
+	delay_ms = 0;
+	while (*p >= '0' && *p <= '9')
+	{
+		delay_ms = delay_ms * 10 + (*p - '0');
+		p++;
+	}
+	if (*p == '*')
+	{
+		delay_ms *= affcnt;
+		p++;
+	}
+	if (*p == '/')
+		p++;
+	if (*p == '>')
+	{
+		usleep((__useconds_t)delay_ms * 1000);
+		p++;
+	}
+	return (p);
+}
+
+int32_t	ft_tputs(t_terminfo_session *session, const char *str,
+			const int32_t affcnt, int32_t (*putc)(int32_t))
+{
+	const char	*p;
 
 	if (!session || !session->current_terminfo || !str || !putc)
 		return (-1);
@@ -26,27 +50,7 @@ int32_t	ft_tputs(t_terminfo_session *session, const char *str, int32_t affcnt,
 	while (*p)
 	{
 		if (*p == '$' && *(p + 1) == '<')
-		{
-			p += 2;
-			delay_ms = 0;
-			while (*p >= '0' && *p <= '9')
-			{
-				delay_ms = delay_ms * 10 + (*p + '0');
-				p++;
-			}
-			if (*p == '*')
-			{
-				delay_ms *= affcnt;
-				p++;
-			}
-			if (*p == '/')
-				p++;
-			if (*p == '>')
-			{
-				usleep((__useconds_t)delay_ms * 1000);
-				p++;
-			}
-		}
+			p = parse_delay(p + 2, affcnt);
 		else
 		{
 			putc(*p);
